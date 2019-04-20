47646d285c7a6f5892f6dcf80a6d67e411301e8c
liuspencersjtu <liusjtu@163.com>
2019-04-20 16:04:22 -0400

    adding manager pager. using waterfall style to present information

--- project/flight_info_system/login/urls.py
+++ project/flight_info_system/login/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('manager', views.manager, name='manager'),
+
 ]
--- project/flight_info_system/login/views.py
+++ project/flight_info_system/login/views.py
@@ -7,3 +7,18 @@ def index(request):
         password = request.POST.get('password')
         print(username, password)
     return render(request, 'index.html', {'name':'Login Page'})
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+    return render(request, 'home.html')
+
+
+def manager(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+    return render(request, 'manager.html')
